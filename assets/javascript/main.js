05fbb7553650526a3fef608a705faf01d121a393
SeanB333 <spb4578@gmail.com>
2018-08-29 21:52:12 -0500

    edit html and js

---
+++ assets/javascript/main.js
@@ -0,0 +1,20 @@
+
+var pcPossible = ["math", "science", "knowledge", "wisdom", "understanding"];
+var displayArea = [];
+var randomWord = pcPossible[Math.floor(Math.random() * pcPossible.length)];
+var displayRandom;
+
+
+
+
+function startGame() {
+    
+   console.log(pcPossible) 
+
+}
+
+
+
+
+
+
